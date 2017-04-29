b4b20f742a45b828e4725ac9b40524b05ad2e4ed
Thomas Reto Strub <ths@strubt.ch>
2017-04-29 21:24:04 +0200

    compress morse, add more GPS info

--- TinyDuino/TinyDuino.ino
+++ TinyDuino/TinyDuino.ino
@@ -10,19 +10,25 @@ void ledShowChar(char c);
 ///  GPS  ///  GPS  ///  GPS  ///
 
 #define _GPS_NO_STATS
+#define _GPS_MIN_INFO
 
 #include <SoftwareSerial.h>
 #include "TinyGPS.h"
 
 typedef struct {
   unsigned short nofSatellites;
-  long latitude, longitude, altitude;
-  unsigned long date, time;
+  float latitude, longitude;
+#ifndef _GPS_MIN_INFO
+  float altitude, course, speedKmph, fromHome;
+#endif
+  int year;
+  byte month, day, hour, minute, second;
 } GpsData;
 
 void gpsWakeUp();
-bool gpsTryReadData(GpsData* data);
 void gpsShutDown();
+void gpsDelay(const unsigned long ms);
+bool gpsTryReadData(GpsData* data);
 
 ///  SD   ///  SD   ///  SD   ///
 
@@ -48,12 +54,37 @@ void setup() {
   if (!success)
     Serial.println("error!");
   else {
-    Serial.println(data.nofSatellites);
-    Serial.println(data.latitude);
-    Serial.println(data.longitude);
+    Serial.print("GPS result: # satellites: ");
+    Serial.print(data.nofSatellites);
+    Serial.print(" latitude: ");
+    Serial.print(data.latitude);
+    Serial.print(" longitude: ");
+    Serial.print(data.longitude);
+#ifdef _GPS_MIN_INFO
+    Serial.println();
+#else
+    Serial.print(" altitude: ");
     Serial.println(data.altitude);
-    Serial.println(data.date);
-    Serial.println(data.time);
+    Serial.print("course: ");
+    Serial.print(data.course);
+    Serial.print(" speed (km/h): ");
+    Serial.print(data.speedKmph);
+    Serial.print(" from home (m): ");
+    Serial.println(data.fromHome);
+#endif
+    Serial.print("date: ");
+    Serial.print(data.year);
+    Serial.print('-');
+    Serial.print(data.month);
+    Serial.print('-');
+    Serial.print(data.day);
+    Serial.print('T');
+    Serial.print(data.hour);
+    Serial.print(':');
+    Serial.print(data.minute);
+    Serial.print(':');
+    Serial.print(data.second);
+    Serial.println('Z');
   }
 }
 
@@ -69,16 +100,15 @@ void loop() {
 ///  LED  ///  LED  ///  LED  ///
 /////////////////////////////////
 
-const int32_t morseSpeed = 100;
-const int32_t charMin = 'a';
-const int32_t charMax = 'z';
-const int32_t morse[26][4] = {
-  /*A*/ { 1, 3 },       /*B*/ { 3, 1, 1, 1 }, /*C*/ { 3, 1, 3, 1 }, /*D*/ { 3, 1, 1 },    /*E*/ { 1 },
-  /*F*/ { 1, 1, 3, 1 }, /*G*/ { 3, 3, 1 },    /*H*/ { 1, 1, 1, 1 }, /*I*/ { 1, 1 },       /*J*/ { 1, 3, 3, 3 },
-  /*K*/ { 3, 1, 3 },    /*L*/ { 1, 3, 1, 1 }, /*M*/ { 3, 3 },       /*N*/ { 3, 1 },       /*O*/ { 3, 3, 3 },
-  /*P*/ { 1, 3, 3, 1 }, /*Q*/ { 3, 3, 1, 3 }, /*R*/ { 1, 3, 1 },    /*S*/ { 1, 1, 1 },    /*T*/ { 3 },
-  /*U*/ { 1, 1, 3 },    /*V*/ { 1, 1, 1, 3 }, /*W*/ { 1, 3, 3 },    /*X*/ { 3, 1, 1, 3 }, /*Y*/ { 3, 1, 3, 3 },
-  /*Z*/ { 3, 3, 1, 1 }
+#define MORSE_SPEED (100)
+#define CHAR_FIRST ('a')
+#define CHAR_LAST ('z')
+const uint8_t morseCodes[] = {
+  /*A*/ (uint8_t)0x70, /*B*/ (uint8_t)0xD5, /*C*/ (uint8_t)0xDD, /*D*/ (uint8_t)0xD4, /*E*/ (uint8_t)0x40, /*F*/ (uint8_t)0x5D,
+  /*G*/ (uint8_t)0xF4, /*H*/ (uint8_t)0x55, /*I*/ (uint8_t)0x50, /*J*/ (uint8_t)0x7F, /*K*/ (uint8_t)0xDC, /*L*/ (uint8_t)0x75,
+  /*M*/ (uint8_t)0xF0, /*N*/ (uint8_t)0xD0, /*O*/ (uint8_t)0xFC, /*P*/ (uint8_t)0x7D, /*Q*/ (uint8_t)0xF7, /*R*/ (uint8_t)0x74,
+  /*S*/ (uint8_t)0x54, /*T*/ (uint8_t)0xC0, /*U*/ (uint8_t)0x5C, /*V*/ (uint8_t)0x57, /*W*/ (uint8_t)0x7C, /*X*/ (uint8_t)0xD7,
+  /*Y*/ (uint8_t)0xDF, /*Z*/ (uint8_t)0xF5
 };
 
 void ledSetup() { pinMode(LED_BUILTIN, OUTPUT); }
@@ -86,13 +116,13 @@ void ledSetup() { pinMode(LED_BUILTIN, OUTPUT); }
 void ledOn() {  digitalWrite(LED_BUILTIN, HIGH); }
 void ledOff() { digitalWrite(LED_BUILTIN, LOW); }
 
-void ledShowString(const char *s) {
+void ledShowString(const char* s) {
 
   for (auto i = 0; s[i] != 0; i++) {
     if (s[i] == ' ')
-      delay(morseSpeed * (7 - 3)); //exclude the next three units
+      delay(MORSE_SPEED * (7 - 3)); //exclude the next three units
     else {
-      if (i > 0) delay(morseSpeed * 3);
+      if (i > 0) delay(MORSE_SPEED * 3);
       ledShowChar(s[i]);
     }
   }
@@ -100,14 +130,16 @@ void ledShowString(const char *s) {
 
 void ledShowChar(const char c) {
 
-  if (charMin > c || c > charMax)
+  if (CHAR_FIRST > c || c > CHAR_LAST)
     return;
 
-  auto m = morse[c - charMin];
-  for (auto i = 0; i < 4 && m[i] != 0; i++) {
-    if (i > 0) delay(morseSpeed);
+  auto m = morseCodes[c - CHAR_FIRST];
+  for (uint8_t i = 0, j = 0b11000000, k = 6; i < 4; i++, j >>= 2, k -= 2) {
+    auto n = (m & j) >> k;
+    if (n == 0) break;
+    if (i > 0) delay(MORSE_SPEED);
     ledOn();
-    delay(morseSpeed * m[i]);
+    delay(MORSE_SPEED * n);
     ledOff();
   }
 }
@@ -116,21 +148,71 @@ void ledShowChar(const char c) {
 ///  GPS  ///  GPS  ///  GPS  ///
 /////////////////////////////////
 
-const auto gpsOnOffPin = A3;
-const auto gpsSysOnPin = A2;
-const uint32_t gpsBaud = 9600;
+#define GPS_TX_PIN (A0)
+#define GPS_RX_PIN (A1)
+#define GPS_SYS_ON_PIN (A2)
+#define GPS_ON_OFF_PIN (A3)
 
-SoftwareSerial gpsSerial(A1, A0);
+#define GPS_CALIBRATION_TIME (20000)
+
+#define HOME_LATITUDE (47.16628)
+#define HOME_LONGITUDE (7.63498)
+
+SoftwareSerial gpsSerial(GPS_RX_PIN, GPS_TX_PIN);
 TinyGPS gps;
 
 void gpsSetup() {
 
-  gpsSerial.begin(gpsBaud);
+  gpsSerial.begin(9600);
 
-  digitalWrite(gpsOnOffPin, LOW);
-  pinMode(gpsOnOffPin, OUTPUT);
-  pinMode(gpsSysOnPin, INPUT);
+  digitalWrite(GPS_ON_OFF_PIN, LOW);
+  pinMode(GPS_ON_OFF_PIN, OUTPUT);
+  pinMode(GPS_SYS_ON_PIN, INPUT);
   delay(100);
+}
+
+void gpsWakeUp() {
+
+  Serial.print("Waking up GPS module..");
+  while (digitalRead(GPS_SYS_ON_PIN) == LOW) {
+    Serial.print(".");
+    digitalWrite(GPS_ON_OFF_PIN, HIGH);
+    delay(5);
+    digitalWrite(GPS_ON_OFF_PIN, LOW);
+    delay(100);
+  }
+  Serial.println(" done.");
+
+  Serial.print("Calibrating GPS module...");
+  gpsDelay(GPS_CALIBRATION_TIME);
+  Serial.println(" done.");
+
+#ifndef _GPS_NO_STATS
+  unsigned long encodedChars;
+  unsigned short goodSentences, failedChecksums;
+  gps.stats(&encodedChars, &goodSentences, &failedChecksums);
+
+  Serial.print("GPS statistics: chars: ");
+  Serial.print(encodedChars);
+  Serial.print(", good sentences: ");
+  Serial.print(goodSentences);
+  Serial.print(", failed checksums: ");
+  Serial.print(failedChecksums);
+  Serial.println('.');
+#endif
+}
+
+void gpsShutDown() {
+
+  Serial.print("Shutting down GPS module..");
+  while (digitalRead(GPS_SYS_ON_PIN) == HIGH) {
+    Serial.print(".");
+    digitalWrite(GPS_ON_OFF_PIN, LOW);
+    delay(5);
+    digitalWrite(GPS_ON_OFF_PIN, HIGH);
+    delay(100);
+  }
+  Serial.println(" done.");
 }
 
 void gpsDelay(const unsigned long ms) {
@@ -142,48 +224,31 @@ void gpsDelay(const unsigned long ms) {
   } while (millis() - start < ms);
 }
 
-void gpsWakeUp() {
-
-  Serial.print("Waking up GPS module..");
-  while (digitalRead(gpsSysOnPin) == LOW) {
-    Serial.print(".");
-    digitalWrite(gpsOnOffPin, HIGH);
-    delay(5);
-    digitalWrite(gpsOnOffPin, LOW);
-    delay(100);
-  }
-  Serial.println(" done.");
-
-  Serial.print("Calibrating GPS module...");
-  gpsDelay(20000);
-  Serial.println(" done.");
-}
-
 bool gpsTryReadData(GpsData* data) {
 
-  unsigned long age;
+  byte hundreth;
+  unsigned long age1, age2;
 
   data->nofSatellites = gps.satellites();
-  gps.get_position(&data->latitude, &data->longitude, &age);
-  data->altitude = gps.altitude();
-  gps.get_datetime(&data->date, &data->time, &age);
+  gps.f_get_position(&data->latitude, &data->longitude, &age1);
+#ifndef _GPS_MIN_INFO
+  data->altitude = gps.f_altitude();
+  data->course = gps.f_course();
+  data->speedKmph = gps.f_speed_kmph();
+  data->fromHome = gps.distance_between(data->latitude, data->longitude, HOME_LATITUDE, HOME_LONGITUDE);
+#endif
+  gps.crack_datetime(&data->year, &data->month, &data->day, &data->hour, &data->minute, &data->second, &hundreth, &age2);
 
-  return data->latitude != TinyGPS::GPS_INVALID_F_ANGLE
-    && data->altitude != TinyGPS::GPS_INVALID_ALTITUDE
-    && age != TinyGPS::GPS_INVALID_AGE;
-}
-
-void gpsShutDown() {
-
-  Serial.print("Shutting down GPS module..");
-  while (digitalRead(gpsSysOnPin) == HIGH) {
-    Serial.print(".");
-    digitalWrite(gpsOnOffPin, LOW);
-    delay(5);
-    digitalWrite(gpsOnOffPin, HIGH);
-    delay(100);
-  }
-  Serial.println(" done.");
+  return data->nofSatellites != TinyGPS::GPS_INVALID_SATELLITES
+    && data->latitude != TinyGPS::GPS_INVALID_F_ANGLE
+    && data->longitude != TinyGPS::GPS_INVALID_F_ANGLE
+#ifndef _GPS_MIN_INFO
+    && data->altitude != TinyGPS::GPS_INVALID_F_ALTITUDE
+    && data->course != TinyGPS::GPS_INVALID_F_ANGLE
+    && data->speedKmph != TinyGPS::GPS_INVALID_F_SPEED
+#endif
+    && age1 != TinyGPS::GPS_INVALID_AGE
+    && age2 != TinyGPS::GPS_INVALID_AGE;
 }
 
 /////////////////////////////////
